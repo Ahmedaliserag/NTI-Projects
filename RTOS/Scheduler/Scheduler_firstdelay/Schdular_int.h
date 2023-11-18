e7a750de83b78f0c31d3a4d418f0096d114b6033
ahmed serag <ahmedalisrag@gmail.com>
2023-11-18 14:02:41 +0200

    updated fristdelay

---
+++ RTOS/Scheduler/Scheduler_firstdelay/Schdular_int.h
@@ -0,0 +1,15 @@
+/*
+ * Schdular_int.h
+ *
+ * Created: 11/17/2023 1:18:11 PM
+ *  Author: Softlaptop
+ */ 
+
+
+#ifndef SCHDULAR_INT_H_
+#define SCHDULAR_INT_H_
+void Create_task (u8 priority ,u16 locad_periodicity ,void(*pf)(void),u8 local_delay);
+void Scheduler (void);
+#define NO_OF_TASKS 3
+
+#endif /* SCHDULAR_INT_H_ */
